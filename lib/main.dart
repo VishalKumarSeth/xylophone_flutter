aee09e1f30b332974c5b595a998ec4dfc926e48b
Vishal Kumar <vishalk@dewsolutions.in>
2022-10-21 18:24:59 +0530

    added expanded , audio player

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const XylophoneApp());
@@ -7,12 +8,101 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red)),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.orange)),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note2.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.yellow)),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note3.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green)),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note4.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.teal)),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note5.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note6.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.purple)),
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note7.wav'));
+                  },
+                  child: Text(''),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
